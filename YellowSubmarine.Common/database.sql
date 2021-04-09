2f8c3f66f72d5faa0b507d8e58d3f9ee57bcde6b
nhill <nhill@MININT-CU56C8E>
2021-04-09 13:59:17 +0100

    added content length to db

--- YellowSubmarine.Common/database.sql
+++ YellowSubmarine.Common/database.sql
@@ -21,13 +21,13 @@ GO
 USE [seneca-log-db]
 GO
 
-SELECT [dbo].[ChkPathHash] ('///1P+STjeUoxHMXxmTvCvA==')
+SELECT [dbo].[ChkPathHash] ('///1P+STjeUoxHMXxmTvCA==')
 GO
 
 
 
 /****** Script for SelectTopNRows command from SSMS  ******/
-SELECT Top(1) [Id]
+SELECT [Id]
       ,[PathHash]
       ,[RequestId]
       ,[Path]
@@ -36,33 +36,28 @@ SELECT Top(1) [Id]
       ,[ResultType]
       ,[Acls]
       ,[ETag]
-  FROM [dbo].[ProcessingLog] where requestid='2ba056df-e387-4fb9-856a-e569bbe252b4'
+  FROM [dbo].[ProcessingLog] where requestid='c1de3957-45b0-4ed2-ab31-5231cf1dc43c'
 
   select distinct(resulttype) from [dbo].[ProcessingLog]
 
-DECLARE	@return_value int
-EXEC	@return_value = [dbo].[CheckPathHash] @PathHash = N'//07G9oFivHPwsfYEnkYVQ=='
-SELECT	'Return Value' = @return_value
-GO
+--truncate table [dbo].[ProcessingLog]
 
-truncate table [dbo].[ProcessingLog]
-
-CREATE PROCEDURE [dbo].[UpsertLog]  @PathHash NVARCHAR(50), @RequestId NVARCHAR(50), @Path NVARCHAR(1024), @ResultType nvarchar(10), @Acls nvarchar(max), @ETag nvarchar(20) AS 
+CREATE PROCEDURE [dbo].[UpsertLog]  @PathHash NVARCHAR(50), @RequestId NVARCHAR(50), @Path NVARCHAR(1024), @ResultType nvarchar(10), @Acls nvarchar(max), @ETag nvarchar(20), @ContentLength bigint AS 
 SET NOCOUNT ON 
 
 MERGE [dbo].ProcessingLog AS [Target]
 USING (SELECT PathHash = @PathHash) AS [Source] 
     ON [Target].PathHash = [Source].PathHash  --- specifies the condition
 WHEN MATCHED THEN
-  UPDATE SET [Target].[ProcessingDateTime] = GetDate(), [Target].[NumberOfAttempts] = [Target].[NumberOfAttempts] + 1, [Target].[ResultType] = @ResultType, [Target].[Acls] = @Acls, [Target].[ETag] =@ETag  --UPDATE STATEMENT
+  UPDATE SET [Target].[ProcessingDateTime] = GetDate(), [Target].[NumberOfAttempts] = [Target].[NumberOfAttempts] + 1, [Target].[ResultType] = @ResultType, [Target].[Acls] = @Acls, [Target].[ETag] = @ETag, [Target].[ContentLength] = @ContentLength  --UPDATE STATEMENT
 WHEN NOT MATCHED THEN
-  INSERT (PathHash, RequestId, [Path], [ProcessingDateTime],[NumberOfAttempts],[ResultType], Acls, Etag) VALUES (@PathHash, @RequestId, @Path, GetDate(), 1, @ResultType, @Acls, @ETag); --INSERT STATEMENT
+  INSERT (PathHash, RequestId, [Path], [ProcessingDateTime],[NumberOfAttempts],[ResultType], Acls, Etag, ContentLength) VALUES (@PathHash, @RequestId, @Path, GetDate(), 1, @ResultType, @Acls, @ETag, @ContentLength); --INSERT STATEMENT
  
 GO
 
 Insert into ProcessingLog (PathHash, RequestId, Path, ProcessingDateTime, NumberOfAttempts, ResultType, Acls, ETag)  VALUES ('pathhash', 'manual', 'parent32/0000_Depth31_Branch1','2021-04-03 15:12:53.424', 4, 'FILE', 'this is the acls', 'ETag')
 
-delete from ProcessingLog
+delete from ProcessingLog where requestid='5214b7e1-5491-4432-a24c-e3ccd0fdb62f'
 
 
 DECLARE	@return_value int
@@ -77,10 +72,15 @@ EXEC	@return_value = [dbo].[UpsertLog]
 
 SELECT	'Return Value' = @return_value
 
-GO
-select * from ProcessingLog where requestid='2ba056df-e387-4fb9-856a-e569bbe252b4'
+select * from ProcessingLog where PathHash='UWjAeF8Mq7OY4lQFS9ZKFw=='
+select  Top (20)* from ProcessingLog where requestid='5b84e540-a84d-491e-affe-b631263a7c5e'
+select max(path) from ProcessingLog where requestid='5b84e540-a84d-491e-affe-b631263a7c5e'
 
-select count(*), min(ProcessingDateTime), max(ProcessingDateTime), max(NumberOfAttempts) from ProcessingLog where requestid='2ba056df-e387-4fb9-856a-e569bbe252b4'
+select count(*) as [Count], min(ProcessingDateTime) as [Start], max(ProcessingDateTime) as [End], max(NumberOfAttempts) as [Max Attempts], sum(NumberOfAttempts) as [Total Attempts] 
+from ProcessingLog 
+where requestid='d28884ec-2185-466e-9543-ea43de10073b'
+
+select distinct(requestid) from ProcessingLog
 /****** Object:  Table [dbo].[ProcessingLog1]    Script Date: 05/04/2021 16:57:51 ******/
 SET ANSI_NULLS ON
 GO
@@ -97,11 +97,61 @@ CREATE TABLE [dbo].[ProcessingLog](
 	[NumberOfAttempts] INT,
 	[ResultType] [nvarchar](10) not null,
 	[Acls] [nvarchar](max) null,
-	[ETag] [nvarchar](20) null
+	[ETag] [nvarchar](20) null,
+	[ContentLength] bigint null
+
 PRIMARY KEY CLUSTERED 
 (
 	[PathHash] ASC
 )WITH (STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY]
 GO
+
+ALTER TABLE ProcessingLog
+ADD ContentLength bigint;
+
+
+CREATE TABLE [dbo].[PageRequestRecord](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[PageRequestKey] [nvarchar](50) not null,
+PRIMARY KEY CLUSTERED 
+(
+	[PageRequestKey] ASC
+)WITH (STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+CREATE FUNCTION ChkPage ( @PageRequestKey NVARCHAR(50) )
+  RETURNS INT
+  AS
+  BEGIN
+      DECLARE @Present int = 0
+        IF EXISTS( Select PageRequestKey From [dbo].[PageRequestRecord] Where PageRequestKey=@PageRequestKey) Set @Present = 1   
+      RETURN(@Present)
+  END
+GO
+
+CREATE PROCEDURE [dbo].[LogPageCompletion]  @PageRequestKey NVARCHAR(50) AS 
+SET NOCOUNT ON 
+
+MERGE [dbo].[PageRequestRecord] AS [Target]
+USING (SELECT PageRequestKey = @PageRequestKey) AS [Source] 
+    ON [Target].PageRequestKey = [Source].PageRequestKey  --- specifies the condition
+WHEN NOT MATCHED THEN
+  INSERT (PageRequestKey) VALUES (@PageRequestKey); --INSERT STATEMENT
+GO
+
+USE [seneca-log-db]
+GO
+
+
+DECLARE	@return_value int
+EXEC	@return_value = [dbo].[LogPageCompletion]
+		@PageRequestKey = 'PageRequestKey7'
+SELECT	'Return Value' = @return_value
+
+select count(*) from PageRequestRecord 
+select *  from PageRequestRecord 
+--delete from PageRequestRecord
+SELECT [dbo].ChkPage ('PageRequestKey7')
 
